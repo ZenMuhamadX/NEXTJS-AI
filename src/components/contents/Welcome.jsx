57e7c30c70d88041a48519bf59b07af33125086b
muhamadzen669 <muhamadzen669@gmail.com>
2024-06-18 06:59:16 +0000

    upate

--- src/components/contents/Welcome.jsx
+++ src/components/contents/Welcome.jsx
@@ -7,7 +7,7 @@ const Welcome = () => {
             <div className="bg-black border rounded-md border-colors-primary p-7 mt-16 w-1/2 h-auto">
                <div className="text-colors-primary text-start font-bold text-xl ">
                   <h1>Selamat Datang!</h1>
-                  <p>Ini adalah halaman selamat datang.</p>
+                  <p>Ini adalah halaman selamat datang kontol</p>
                </div>
             </div>
          </div>
