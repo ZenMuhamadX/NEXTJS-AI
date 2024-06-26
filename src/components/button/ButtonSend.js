3c3f1fed3b0bbf24863b060ea4ed58bc0e9d0674
muhamadzen669 <muhamadzen669@gmail.com>
2024-06-26 04:13:23 +0000

    fix bug

--- src/components/button/ButtonSend.js
+++ src/components/button/ButtonSend.js
@@ -1,11 +1,10 @@
 export const handleInput = async (prompt) => {
    if (!prompt) return;
-   const token = process.env.NEXT_PUBLIC_TOKEN_JWT;
    const get = await fetch("https://api.zenxyz.online/v2/ai/gemini", {
       method: "POST",
       headers: {
          "content-type": "application/json",
-         authorization: `Bearer ${token}`,
+         authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VybmFtZSI6Im11aGFtYWQgbnVyIGZhdXphbiIsImVtYWlsIjoibXVoYW1hZHplbjY2OUBnbWFpbC5jb20iLCJpYXQiOjE3MTkxNTQ0NDd9.IS0-H4PJY_5IxyDFfQ6_KeYJh1FGtvUkly-XnWfsCwM`,
       },
       body: prompt,
    });
--- src/components/button/clearState.js
+++ src/components/button/clearState.js
@@ -1,11 +1,10 @@
 export const clearState = async (setChatHistory) => {
    setChatHistory([]);
-   const token = process.env.NEXT_PUBLIC_TOKEN_JWT;
    await fetch("https://api.zenxyz.online/v2/ai/clear", {
       method: "POST",
       headers: {
          "content-type": "application/json",
-         authorization: `Bearer ${token}`,
+         authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VybmFtZSI6Im11aGFtYWQgbnVyIGZhdXphbiIsImVtYWlsIjoibXVoYW1hZHplbjY2OUBnbWFpbC5jb20iLCJpYXQiOjE3MTkxNTQ0NDd9.IS0-H4PJY_5IxyDFfQ6_KeYJh1FGtvUkly-XnWfsCwM`,
       },
    });
 };
